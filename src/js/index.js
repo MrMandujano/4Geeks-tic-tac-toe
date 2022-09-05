dd172b1e6eef520d63ff468e34d02c52a891fd81
Francisco Mandujano <93405601+MrMandujano@users.noreply.github.com>
2022-09-05 11:36:17 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+// include your styles into the webpack bundle
+import "../styles/index.css";
+
+//import your own components
+import Home from "./component/home.jsx";
+import Game from "./component/Game"
+
+//render your react application
+ReactDOM.render(<Game />, document.querySelector("#app"));
